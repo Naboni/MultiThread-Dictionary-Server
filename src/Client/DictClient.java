a92379dcf2b02ae1d2e42c77203ab1d35c323c09
Naboni <se.naboni.abebe@gmail.com>
2021-12-29 21:49:18 +0300

    MultiThread

---
+++ src/Client/DictClient.java
@@ -0,0 +1,129 @@
+package Client;
+import java.util.concurrent.TimeoutException;
+import StateCode.StateCode;
+
+public class DictClient {
+	private String address;
+	private int port;
+	private int operationCount = 0;
+	private DictClientGUI ui;
+
+	public static void main(String[] args) {
+		try {
+			// Check port format.
+			if (Integer.parseInt(args[1]) <= 1024 || Integer.parseInt(args[1]) >= 49151) {
+				System.out.println("Invalid Port Number: Port number should be between 1024 and 49151!");
+				System.exit(-1);
+			}
+			System.out.println("Dictionary Client");
+			DictClient client = new DictClient(args[0], Integer.parseInt(args[1]));
+			client.run();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Lack of Parameters:\nPlease run like \"java -java DictClient.java <server-address> <server-port>");
+			e.printStackTrace();
+		} catch (NumberFormatException e) {
+			System.out.println("Invalid Port Number: Port number should be between 1024 and 49151!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public DictClient(String address, int port) {
+		this.address = address;
+		this.port = port;
+		this.operationCount = 0;
+		ui = null;
+	}
+	
+	public void run() {
+		try {
+			this.ui = new DictClientGUI(this);
+			ui.getFrame().setVisible(true);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Please enter <server-address> <server-port>");
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void addLog(int state, String word, String meaning) {
+		System.out.println("--LOG: " + String.valueOf(operationCount) + " ------");
+		System.out.println("  Request:");
+		switch (state) {
+		case StateCode.ADD:
+			System.out.println("  Command: ADD");
+			break;
+		case StateCode.QUERY:
+			System.out.println("  Command: QUERY");
+			break;
+		case StateCode.REMOVE:
+			System.out.println("  Command: REMOVE");
+			break;
+		default:
+			System.out.println("  Error: Unknown Command");
+			break;
+		}
+		System.out.println("  Word: " + word);
+		if (state == StateCode.ADD) System.out.println("  Meaning:\n\t" + meaning);
+		operationCount++;
+	}
+	
+	private void printResponse(int state, String meaning) {
+		System.out.println("  Response:");
+		switch (state) {
+		case StateCode.SUCCESS:
+			System.out.println("  State: SUCCESS");
+			break;
+		case StateCode.FAIL:
+			System.out.println("  State: FAIL");
+			break;
+		default:
+			System.out.println("  Error: Unknown State");
+			break;
+		}
+		System.out.println("  Meaning:\n\t" + meaning);
+	}
+	
+	public int add(String word, String meaning) {
+		String[] resultArr = execute(StateCode.ADD, word, meaning);
+		return Integer.parseInt(resultArr[0]);
+	}
+	
+	public int remove(String word) {
+		String[] resultArr = execute(StateCode.REMOVE, word, "");
+		return Integer.parseInt(resultArr[0]);
+	}
+	
+	public String[] query(String word) {
+		String[] resultArr = execute(StateCode.QUERY, word, "");
+		return resultArr;
+	}
+	
+	private String[] execute(int command, String word, String meaning) {
+		int state = StateCode.FAIL;
+		addLog(command, word, meaning);
+		try {
+			System.out.println("Trying to connect to server...");
+			ExecuteThread eThread = new ExecuteThread(address, port, command, word, meaning);
+			eThread.start();
+			eThread.join(2000);
+			if (eThread.isAlive()) {
+				eThread.interrupt();
+				throw new TimeoutException();
+			}
+			String[] eThreadResult = eThread.getResult();
+			state = Integer.parseInt(eThreadResult[0]);
+			meaning = eThreadResult[1];
+			System.out.println("Connect Success!");
+		} catch (TimeoutException e) {
+			state = StateCode.TIMEOUT;
+			meaning = "";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		printResponse(state, meaning);
+		String[] resultArr = {String.valueOf(state), meaning};
+		return resultArr;
+	}
+}
---
+++ src/Client/DictClientGUI.java
@@ -0,0 +1,181 @@
+package Client;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import StateCode.StateCode;
+
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+import java.awt.*;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+
+public class DictClientGUI {
+
+	private JFrame frame;
+	private JTextArea meaningPane;
+	private JTextField wordField;
+	private DictClient dictClient;
+	
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	public DictClientGUI(DictClient client) {
+		dictClient =client;
+		initialize();
+	}
+	
+	private Boolean isValid(String word, String meaning, int command) {
+		if (word.equals("")) {
+			JOptionPane.showMessageDialog(frame, "Word is required.", "Warning", JOptionPane.WARNING_MESSAGE);
+			return false;
+		} else if (command == StateCode.ADD && meaning.equals("")) {
+			JOptionPane.showMessageDialog(frame, "Word meaning is required.", "Warning", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		return true;
+	}
+
+	private void initialize() {
+		frame = new JFrame("Client: Word dictionary");
+		frame.setMinimumSize(new Dimension(450, 340));
+		frame.setBounds(635, 340, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(Color.CYAN);
+		wordField = new JTextField();
+		wordField.setColumns(10);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String word = wordField.getText();
+				String meaning = meaningPane.getText();
+				if (isValid(word, meaning, StateCode.ADD)) {
+					int confirm = JOptionPane.showConfirmDialog(frame,  "Confirm to Add a new word?", "Confirm Window", JOptionPane.YES_NO_OPTION);
+					if (confirm == JOptionPane.YES_OPTION) {
+						int state = dictClient.add(word, meaning);
+						if(state == StateCode.UNKNOWN_HOST) {
+							JOptionPane.showMessageDialog(frame, "Unknown Host!\nPlease restart with a correct Address and IP.", 
+									"Warning", JOptionPane.ERROR_MESSAGE);
+						} else if (state == StateCode.FAIL) {
+							JOptionPane.showMessageDialog(frame, "Word Exist!", "Warning", JOptionPane.WARNING_MESSAGE);
+						} else if (state == StateCode.TIMEOUT) {
+							JOptionPane.showMessageDialog(frame, "Timeout!\nPlease check the server or restart with a correct Address and IP.", 
+									"Warning", JOptionPane.ERROR_MESSAGE);
+						} else {
+							JOptionPane.showMessageDialog(frame, "Add Success!", "Tips", JOptionPane.INFORMATION_MESSAGE);
+						}
+					}
+				}
+			}
+		});
+		
+		JButton btnQuery = new JButton("Query");
+		btnQuery.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String word = wordField.getText();
+				if (isValid(word, "", StateCode.QUERY)) {
+					String[] resultArr = dictClient.query(word);
+					int state = Integer.parseInt(resultArr[0]);
+					if (state == StateCode.UNKNOWN_HOST) {
+						JOptionPane.showMessageDialog(frame, "Unknown Host!\nPlease restart with a correct Address and IP.", "Warning", JOptionPane.ERROR_MESSAGE);
+					} else if (state == StateCode.FAIL) {
+						JOptionPane.showMessageDialog(frame, "Query Fail\nWord Not Exist!", "Warning", JOptionPane.WARNING_MESSAGE);
+					} else if (state == StateCode.TIMEOUT) {
+						JOptionPane.showMessageDialog(frame, "Timeout!\nPlease check the server or restart with a correct Address and IP.", 
+								"Warning", JOptionPane.ERROR_MESSAGE);
+					} else {
+						meaningPane.setText(resultArr[1]);
+					}
+				}
+			}
+		});
+		
+		JButton btnRemove = new JButton("Remove");
+		btnRemove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String word = wordField.getText();
+				if (isValid(word, "", StateCode.REMOVE)) {
+					int confirm = JOptionPane.showConfirmDialog(frame,  "Confirm to Remove a new word?", "Confirm Window", JOptionPane.YES_NO_OPTION);
+					if (confirm == JOptionPane.YES_OPTION) {
+						int state = dictClient.remove(word);
+						if(state == StateCode.UNKNOWN_HOST) {
+							JOptionPane.showMessageDialog(frame, "Unknown Host!\nPlease restart with a correct Address and IP.", "Warning", JOptionPane.ERROR_MESSAGE);
+						}
+						else if (state == StateCode.FAIL) {
+							JOptionPane.showMessageDialog(frame, "Remove Fail\nWord Not Exist!", "Warning", JOptionPane.WARNING_MESSAGE);
+						} else if (state == StateCode.TIMEOUT) {
+							JOptionPane.showMessageDialog(frame, "Timeout!\nPlease check the server or restart with a correct Address and IP.", 
+									"Warning", JOptionPane.ERROR_MESSAGE);
+						} else {
+							JOptionPane.showMessageDialog(frame, "Remove Success!", "Tips", JOptionPane.INFORMATION_MESSAGE);
+						}
+					}
+				}
+			}
+		});
+		
+		JLabel lblMeaning = new JLabel("The Meaning(s) of the word: ");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JLabel lblWord = new JLabel("Word:");
+		//lblWord.setFont(new Font("Serif", Font.PLAIN, 14));
+		//lblWord.setPreferredSize(new Dimension(250, 100));
+
+		meaningPane = new JTextArea();
+		scrollPane.setViewportView(meaningPane);
+		meaningPane.setLineWrap(true);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(5)
+					.addComponent(lblMeaning, GroupLayout.PREFERRED_SIZE, 244, GroupLayout.PREFERRED_SIZE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(5)
+					.addComponent(lblWord, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+				.addComponent(wordField, GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(5)
+					.addComponent(btnAdd, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+					.addGap(25)
+					.addComponent(btnQuery, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+					.addGap(25)
+					.addComponent(btnRemove, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+					.addGap(5))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(5)
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 439, Short.MAX_VALUE)
+					.addContainerGap())
+
+		);
+
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(5)
+						.addComponent(lblWord, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+						.addComponent(wordField, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+						.addGap(5)
+						.addGap(5)
+						.addComponent(lblMeaning, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+					.addGap(5)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnAdd, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnQuery, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnRemove, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE))
+					.addGap(8))
+		);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+}
